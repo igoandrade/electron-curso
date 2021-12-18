e9e6c431561c9e885b832fda627700244d883c21
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-18 19:11:54 -0300

    Personalização do menu da aplicação

--- App04-Lista-Compras/main.js
+++ App04-Lista-Compras/main.js
@@ -1,10 +1,41 @@
-const { app, BrowserWindown, Menu, ipcMain } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+
+let mainTemplateMenu = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Novo Produto',
+                click() {
+                    createNewProductWindow()
+                }
+            },
+            {
+                label: 'Excluir Produtos',
+                click() {
+                    mainWindow.webContents.send('produto:excluir')
+                }
+            },
+            {
+                label: 'Sair',
+                accelerator: 'CommandOrControl + Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+function createNewProductWindow() {
+
+}
 
 let mainWindow;
 let newProductWindow;
 
 function createMainWindow() {
-    mainWindow = new BrowserWindown({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -13,4 +44,11 @@ function createMainWindow() {
     });
 
     mainWindow.loadFile('index.html');
+
+    let mainMenu = Menu.buildFromTemplate(mainTemplateMenu);
+
+    mainWindow.setMenu(mainMenu);
 }
+
+
+app.whenReady().then(createMainWindow);
